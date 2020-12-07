53cba31ee94b6d06361caa3438e03bc5771006c9
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2020-12-07 20:00:41 +0530

    Preloader, logs, logItem components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
+
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 
 const App = () => {
   useEffect(() => {
@@ -9,7 +12,14 @@ const App = () => {
     M.AutoInit();
   });
 
-  return <div className='App'>My App</div>;
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className='container'>
+        <Logs />
+      </div>
+    </Fragment>
+  );
 };
 
 export default App;
