96f7820035830807e614db677f3c693e8a5ffe4e
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2020-12-09 14:03:42 +0530

    Connecting Redux to components

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,5 +1,18 @@
 import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
 
+// export const getLogs = () => {
+//   return async (dispatch) => {
+//     setLoading();
+//     const res = await fetch('/logs');
+//     const data = await res.json();
+//     dispatch({
+//       type: GET_LOGS,
+//       payload: data,
+//     });
+//   };
+// };
+
+//get logs from server
 export const getLogs = () => async (dispatch) => {
   try {
     setLoading();
