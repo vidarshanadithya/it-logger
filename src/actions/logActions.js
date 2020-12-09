42dbb0c218a945398a050d38cfe23de81622a26d
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2020-12-09 17:03:20 +0530

    [Logs] - Adding Logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types';
 
 // export const getLogs = () => {
 //   return async (dispatch) => {
@@ -20,6 +20,31 @@ export const getLogs = () => async (dispatch) => {
     const data = await res.json();
     dispatch({
       type: GET_LOGS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
+//add new log
+export const addLog = (log) => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_LOG,
       payload: data,
     });
   } catch (err) {
