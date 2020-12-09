e1c7ba9dbf3a5101bea91a660c8dfd34c219632b
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2020-12-09 17:41:29 +0530

    [Logs] - Search Logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -7,6 +7,7 @@ import {
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_LOG,
+  SEARCH_LOGS,
 } from './types';
 
 // export const getLogs = () => {
@@ -105,6 +106,24 @@ export const updateLog = (log) => async (dispatch) => {
     dispatch({
       type: LOGS_ERROR,
       payload: err.response.statusText,
+    });
+  }
+};
+
+//search from server
+export const searchLogs = (text) => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch(`/logs/?q=${text}`);
+    const data = await res.json();
+    dispatch({
+      type: SEARCH_LOGS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
     });
   }
 };
